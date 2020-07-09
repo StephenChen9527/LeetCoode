269aa81d3f39e875db385a9e6ff7e1f0caffd731
StephenChen9527 <cundiandongxi1994@gmail.com>
2020-07-09 18:22:44 +0800

    update

---
+++ src/main/java/com/nullbugs/easy/Exam543.java
@@ -0,0 +1,27 @@
+package com.nullbugs.easy;
+
+import com.nullbugs.util.TreeNode;
+
+public class Exam543 {
+    public static void main(String[] args) {
+
+    }
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int left = deep(root.left);
+        int right = deep(root.right);
+        return left+right;
+    }
+    public int deep(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        int left = diameterOfBinaryTree(root.left);
+        int right = diameterOfBinaryTree(root.right);
+        return Integer.max(left,right)+1;
+    }
+
+}
